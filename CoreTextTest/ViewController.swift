5e57d322b3603213d59a331fa6e8944703fafc19
Icelolli <18625260611@163.com>
2015-08-27 11:37:42 +0800

    Initial Commit

---
+++ CoreTextTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreTextTest
+//
+//  Created by Li Yuan on 8/27/15.
+//  Copyright © 2015 Li Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
